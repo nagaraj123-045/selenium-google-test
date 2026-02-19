e06d0ce75680f95b64d34393e70e0cd20194d7ba
selenium tester <nagarajnaik4428@gmail.com>
2026-02-19 18:23:14 -0500

    test google

---
+++ day2.py
@@ -0,0 +1,22 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
+
+driver=webdriver.Chrome()
+driver.implicitly_wait(10)
+
+driver.get("https://www.google.com")
+
+driver.maximize_window()
+
+wait=WebDriverWait(driver,10)
+serach_box= wait.until(EC.presence_of_element_located((By.NAME,"q")))
+
+#serach_box=driver.find_element(By.NAME,"q")
+serach_box.send_keys("w3 school")
+serach_box.send_keys(Keys.RETURN)
+time.sleep(100)
+driver.quit()
